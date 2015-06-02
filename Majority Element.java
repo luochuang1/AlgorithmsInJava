7aed1427f8c583dc1f9ca5db2e51957adeab34e4
luochuang1 <lh_chuang@163.com>
2015-06-02 17:44:01 +0800

    Update Majority Element.java

--- Majority Element.java
+++ Majority Element.java
@@ -1,8 +1,8 @@
 public class Solution {
 //1. 简单来讲，就是不断对某个议案投票，如果有人有别的议案，则将前面认为的议案的cnt减1，减到0换一个议案。
-如果存在majority number，那么这个议案一定不会被减到0，最后会胜出。
-2. 投票完成后，要对majority number进行检查，以排除不存在majority number的情况。如 1，2，3，4这样的数列，
-是没有majory number的。很简单，统计一下结果议案的票数，没有过半就是没有majority number.
+//如果存在majority number，那么这个议案一定不会被减到0，最后会胜出。
+//2. 投票完成后，要对majority number进行检查，以排除不存在majority number的情况。如 1，2，3，4这样的数列，
+//是没有majory number的。很简单，统计一下结果议案的票数，没有过半就是没有majority number.
     public int majorityElement(int[] nums) {
         int candidate = 0, count = 0;
         for (int i = 0; i < nums.length; i++) {
