ccc6a42be24e9d213ea12d0099a9338b090c0f79
luochuang1 <lh_chuang@163.com>
2015-06-03 00:28:39 +0800

    寻找旋转排序数组中的最小值
    假设一个旋转排序的数组其起始位置是未知的（比如0 1 2 4 5 6 7 可能变成是4 5 6 7 0 1 2）。
    你需要找到其中最小的元素。
    你可以假设数组中不存在重复的元素。

---
+++ Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int findMin(int[] num) {
+       int start = 0, end = num.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (num[mid] >= num[end]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (num[start] < num[end]) {
+            return num[start];
+        } else {
+            return num[end];
+        }
+    }
+}
