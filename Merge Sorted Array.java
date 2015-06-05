95a8e60435f2e34b0b43547c01bbce2f83892e84
luochuang1 <lh_chuang@163.com>
2015-06-05 15:33:49 +0800

    Update Merge Sorted Array.java

--- Merge Sorted Array.java
+++ Merge Sorted Array.java
@@ -1,25 +1,11 @@
-class Solution {
-    /**
-     * @param A: sorted integer array A which has m elements, 
-     *           but size of A is m+n
-     * @param B: sorted integer array B which has n elements
-     * @return: void
-     */
-    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
-        // write your code here
-         int i = m-1, j = n-1, index = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (A[i] > B[j]) {
-                A[index--] = A[i--];
-            } else {
-                A[index--] = B[j--];
-            }
-        }
-        while (i >= 0) {
-            A[index--] = A[i--];
-        }
-        while (j >= 0) {
-            A[index--] = B[j--];
-        }
-    }
+public class Solution {
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int index = m + n - 1, i = m - 1, j = n - 1;
+		while (j >= 0)
+		        //error:indexoutofbound
+			if (i < 0 || nums1[i] < nums2[j])
+				nums1[index--] = nums2[j--];
+			else
+				nums1[index--] = nums1[i--];
+	}
 }
