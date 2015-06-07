fa5610f0b507708e84de0a38445451eab234bf6a
luochuang1 <lh_chuang@163.com>
2015-06-07 20:06:41 +0800

    Create search-in-rotated-sorted-array-ii.java

---
+++ search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean search(int[] A, int target) {
+        for(int i = 0; i <A.length;i++){
+            if(A[i] == target)
+              return true;
+        }
+        return false;
+    }
+}
