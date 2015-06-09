8751b7b8d93eac3ecee5aa082bbed668e5130a28
luochuang1 <lh_chuang@163.com>
2015-06-09 14:01:10 +0800

    Create intersection-of-two-linked-lists.java

---
+++ intersection-of-two-linked-lists.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if( null==headA || null==headB )
+            return null;
+
+        ListNode curA = headA, curB = headB;
+        while( curA!=curB){
+            curA = curA==null?headB:curA.next;
+            curB = curB==null?headA:curB.next;
+        }
+        return curB;
+    }
+}
